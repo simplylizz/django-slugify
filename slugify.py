401db5b1c30199769dab4b79eb0357a8c73972e8
Anton Yanchenko <simplylizz@gmail.com>
2021-11-20 23:05:49 +0100

    Make it working with Django 3.2
    Perhaps it won't work with python2 anymore.

--- slugify.py
+++ slugify.py
@@ -1,20 +1,14 @@
 from django import conf
-from django.utils import encoding
-
-try:
-    # Django 1.5 have some permutations.
-    from django.utils import text as src_pkg
-    from django.utils.text import slugify as dj_slugify
-except ImportError:
-    from django.template import defaultfilters as src_pkg
-    from django.template.defaultfilters import slugify as dj_slugify
+from django.utils import text as src_pkg
+from django.utils.text import slugify as dj_slugify
 
 import unidecode
 
 
-def slugify(value):
-    value = encoding.smart_unicode(value)
-    return dj_slugify(encoding.smart_unicode(unidecode.unidecode(value)))
+def slugify(value, allow_unicode=False):
+    if not allow_unicode:
+        value = unidecode.unidecode(value)
+    return dj_slugify(value=value, allow_unicode=allow_unicode)
 
 
 if getattr(conf.settings, 'PATCH_SLUGIFY', True):
